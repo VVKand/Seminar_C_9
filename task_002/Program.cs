9f466c4a0296a58f0c8f741f001b66d14f25febc
vkand <vvkand@mail.ru>
2023-01-20 19:49:05 +0700

    задания выполнены

---
+++ task_002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное максимальное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное минимальное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SummaRec(N));
+
+int SummaRec(int max)
+{
+    if (max==M) return M;
+    return max + SummaRec(max-1);
+}
